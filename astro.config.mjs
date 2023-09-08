a4ece01096b2ae376fe4a67578ba02ba70b5c7ef
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-09-07 23:13:15 -0700

    Blog, RSS & Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import solid from '@astrojs/solid-js';
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		react({
-			include: ['src/components/add-to-cart.tsx'],
-		}),
-		solid({
-			include: ['src/components/cart.tsx'],
-		}),
-	],
+  site: 'https://astro-sandwich-shop-jpsanantonio.netlify.app',
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true
+  },
+  integrations: [react({
+    include: ['src/components/add-to-cart.tsx']
+  }), solid({
+    include: ['src/components/cart.tsx']
+  })],
+  adapter: netlify()
 });
